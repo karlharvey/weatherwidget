e6cd8a0ce32eaa9b7143fb6114619cfdd7c80175
Karl Harvey <10708411+karlharvey@users.noreply.github.com>
2020-05-08 19:40:13 +0100

    Added geolocation functionality

--- weatherwidget/js/main.js
+++ weatherwidget/js/main.js
@@ -1,53 +1,88 @@
 /*jslint browser:true */
-'use strict';
 
-var weatherConditions = new XMLHttpRequest();
-var weatherForecast = new XMLHttpRequest();
-var cObj;
-var fObj;
+function getLocation() {
+  if (navigator.geolocation) {
+    alert("Geolocation available");
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      var currentUrl =
+        "//api.openweathermap.org/data/2.5/weather?lat=" +
+        lat +
+        "&lon=" +
+        long +
+        "&appid=770c2f41007371eb49fe4da42974d5e4&units=metric";
 
-// GET THE CONDITIONS
-weatherConditions.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=53.33&lon=-6.29&appid=770c2f41007371eb49fe4da42974d5e4&units=metric', true);
-weatherConditions.responseType = 'text';
-weatherConditions.send(null);
+      var forecaseUrl =
+        "//api.openweathermap.org/data/2.5/forecast?lat=" +
+        lat +
+        "&lon=" +
+        long +
+        "&appid=770c2f41007371eb49fe4da42974d5e4&units=metric";
 
-weatherConditions.onload = function() {
-    if (weatherConditions.status === 200){
-        cObj = JSON.parse(weatherConditions.responseText); 
-        console.log(cObj);
-        document.getElementById('location').innerHTML=cObj.name;
-        document.getElementById('weather').innerHTML=cObj.weather[0].description;
-        document.getElementById('temperature').innerHTML=Math.round(cObj.main.temp)+"°C";
-        document.getElementById('desc').innerHTML="Wind Speed: "+cObj.wind.speed;
+      getWeatherData(currentUrl, forecaseUrl);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
 
+function getWeatherData(currentUrl, forecaseUrl) {
+  var weatherConditions = new XMLHttpRequest();
+  var weatherForecast = new XMLHttpRequest();
+  var cObj;
+  var fObj;
+  console.log(currentUrl);
+
+  // GET THE CONDITIONS
+  weatherConditions.open("GET", currentUrl, true);
+  weatherConditions.responseType = "text";
+  weatherConditions.send(null);
+
+  weatherConditions.onload = function () {
+    if (weatherConditions.status === 200) {
+      cObj = JSON.parse(weatherConditions.responseText);
+      console.log(cObj);
+      document.getElementById("location").innerHTML = cObj.name;
+      document.getElementById("weather").innerHTML =
+        cObj.weather[0].description;
+      document.getElementById("temperature").innerHTML =
+        Math.round(cObj.main.temp) + "°C";
+      document.getElementById("desc").innerHTML =
+        "Wind Speed: " + cObj.wind.speed + "mph";
     } //end if
-}; //end function
+  }; //end function
 
-// GET THE FORECARST
-weatherForecast.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?lat=53.33&lon=-6.29&appid=770c2f41007371eb49fe4da42974d5e4&units=metric', true);
-weatherForecast.responseType = 'text'; 
-weatherForecast.send();
+  // GET THE FORECARST
+  weatherForecast.open("GET", forecaseUrl, true);
+  weatherForecast.responseType = "text";
+  weatherForecast.send();
 
-weatherForecast.onload = function() {
-if (weatherForecast.status === 200){
-	fObj = JSON.parse(weatherForecast.responseText);
-    console.log(fObj);
-    
-    for (var i = 0, r = 1; i <= 24; i += 8, r++) {
+  weatherForecast.onload = function () {
+    if (weatherForecast.status === 200) {
+      fObj = JSON.parse(weatherForecast.responseText);
+      console.log(fObj);
+
+      for (var i = 0, r = 1; i <= 24; i += 8, r++) {
         console.log(i);
         console.log(r);
         var date_raw = fObj.list[i].dt_txt;
-        date_raw = date_raw.substring(5,11);
-        document.getElementById('r'+r.toString()+'c1').innerHTML=date_raw;
+        date_raw = date_raw.substring(5, 11);
+        document.getElementById("r" + r.toString() + "c1").innerHTML = date_raw;
 
         var iconcode = fObj.list[i].weather[0].icon;
-        var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png"
-        document.getElementById('r'+r.toString()+'c2').src = icon_path;
-        document.getElementById('r'+r.toString()+'c3').innerHTML = Math.round(fObj.list[i].main.temp_min)+"°C";
-        document.getElementById('r'+r.toString()+'c4').innerHTML = Math.round(fObj.list[i].main.temp_max)+"°C";
+        var icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
+        document.getElementById("r" + r.toString() + "c2").src = icon_path;
+        document.getElementById("r" + r.toString() + "c3").innerHTML =
+          Math.round(fObj.list[i].main.temp_min) + "°C";
+        document.getElementById("r" + r.toString() + "c4").innerHTML =
+          Math.round(fObj.list[i].main.temp_max) + "°C";
+      }
     }
-    
-    /*var date_raw = fObj.list[0].dt_txt;
+  };
+}
+
+/*var date_raw = fObj.list[0].dt_txt;
     date_raw = date_raw.substring(5,11);
     document.getElementById('r1c1').innerHTML=date_raw;
 
@@ -75,8 +110,6 @@ if (weatherForecast.status === 200){
     var icon_path = "http://openweathermap.org/img/w/" + iconcode + ".png"
     document.getElementById('r3c2').src = icon_path;
     document.getElementById('r3c3').innerHTML = Math.round(fObj.list[16].main.temp_min)+"°C";
-    document.getElementById('r3c4').innerHTML = Math.round(fObj.list[16].main.temp_max)+"°C";*/
-} //end if
-}; //end function
-
-
+    document.getElementById('r3c4').innerHTML = Math.round(fObj.list[16].main.temp_max)+"°C";
+  } //end if
+}; //end function*/
